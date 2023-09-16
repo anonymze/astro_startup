e6b4073a56cf761c24e0029d5eba3a6cab6787fd
ymetier <ymetier@openstudio.fr>
2023-09-16 17:09:30 +0200

    startup

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+export default {
+    plugins: ['prettier-plugin-astro'],
+    overrides: [
+        {
+          files: ["*.astro", "*.tsx"],
+          options: {
+            parser: 'astro',
+          },
+        },
+      ],
+    printWidth: 100
+}
---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+declare global {
+    var myString: string;
+    function myFunction(): boolean;
+  }
+  
+  export {};
